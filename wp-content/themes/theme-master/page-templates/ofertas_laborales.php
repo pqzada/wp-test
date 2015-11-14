951b5a6a527a10869cd714206adf2c92b8de0c33
pqzada <pqzada@gmail.com>
2015-11-14 02:30:08 -0300

    Se modifica vista de ofertas laborales

--- wp-content/themes/theme-master/page-templates/ofertas_laborales.php
+++ wp-content/themes/theme-master/page-templates/ofertas_laborales.php
@@ -7,78 +7,98 @@ Template Name: Ofertas Laborales
 <?php get_header(); ?>
 		<div class="container page">
 			<div class="hidden-xs hidden-sm hidden-md col-lg-4"></div>
-			<div class="col-sm-8">
-				<?php
-					$current_category = get_post(10);
-					echo '<h2>'.$current_category->post_title.'</h2>';
-				?>
+			<div class="col-xs-12">
+				<center><h2>Ofertas de empleo destacadas</h2></center>
 			</div>
 		</div>
 
 		<section id="contenido" class="container margin-up-down">
-			<div id="home" class="col-xs-12 col-sm-8 col-lg-8">
+			<div id="home" class="col-xs-12">
 			 	<?php the_post_thumbnail( '', array( 'class' => 'img-responsive' ) ); ?>
-	<?php
-		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-		$args = array(
-			'post_type' => 'ofertalaboral',
-			'paged' => $paged,
-			'posts_per_page' => 9,
-			'meta_query'	=> array(
-				array(
-					'key' => 'fecha_de_cierre',
-					'value' => date('Ymd'),
-					'compare' => '>=',
-				),
-			),
-		);
-		wp_reset_query();
-		$oferta_laboral = query_posts($args);
-		$cont = 1;
-			if (have_posts()) {
-				while (have_posts()) {
-					the_post();
-					// $id_pagina = get_the_id();
-			?>
-			<?php if($cont == 1){ ?>
-					<div id="tabla">
-						<table>
-							<tr class="encabezado">
-								<td>Codigo</td>
-								<td>Trabajo</td>
-								<td>Fecha Cierre</td>
-							</tr>
-	 						<?php foreach ($oferta_laboral as $oferta){ ?>
-							<tr>
-								<td><?php echo str_replace('', '', $oferta->ID); ?></td>
-								<td>
-								<a href="<?php echo get_permalink($oferta->ID); ?>">
-								<?php echo limitarCaracteres($oferta->post_title, 80); ?>
-								</td>
-								</a>
-								<td>
-									<?php
-									if(get_field('fecha_de_cierre')){
-										$date = DateTime::createFromFormat('Ymd', get_field('fecha_de_cierre'));
-										echo $date->format('d/m/Y');
-									}else{
-										echo "Indefinida";
-									}
-									?>
-								</td>
-							</tr>
+				<?php
+					$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+					$args = array(
+						'post_type' => 'ofertalaboral',
+						'paged' => $paged,
+						'posts_per_page' => 9,
+						'meta_query'	=> array(
+							array(
+								'key' => 'fecha_de_cierre',
+								'value' => date('Ymd'),
+								'compare' => '>=',
+							),
+						),
+					);
+					wp_reset_query();
+					$oferta_laboral = query_posts($args);
 
-	 					<?php } ?>
-						</table>
-		 		</div>
-		 		<?php } ?>
-					<?php $cont++;?>
-		 <?php
-	 			}
-	 		}
-		  ?>
-		<?php get_contenido('nav'); ?>
+					$bg = array(0 => 'gray', 1 => 'orange', 2 => 'white');
+
+					$cont = 1;
+					if (have_posts()) {
+						while (have_posts()) {
+							the_post();
+
+							$idx = 0;
+							foreach ($oferta_laboral as $oferta) {								
+								$post_meta = get_post_custom($oferta->ID);
+?>
+
+<div class="row oferta_laboral <?php echo $bg[$idx%3]; ?>">
+
+	<div class="col-xs-7 title">
+		<a href="<?php echo get_permalink($oferta->ID); ?>">
+			<?php echo $oferta->post_title; ?>
+		</a>
 	</div>
-<?php get_sidebar(); ?>
+
+	<div class="col-xs-5 tecnologias">
+		<?php 
+			if(isset($post_meta['tecnologias'])) {
+				echo "Tecnologías: " . implode(", ", unserialize($post_meta['tecnologias'][0]));
+			}
+		?>
+	</div>
+
+	<div class="col-xs-7 info">
+		<?php 
+			if(isset($post_meta['disponibilidad'])) {
+				echo "Disponibilidad: " . $post_meta['disponibilidad'][0];
+			}
+
+			if(isset($post_meta['sueldo']) && $post_meta['sueldo'][0] != "") {
+				echo " | " . $post_meta['sueldo'][0];
+			}
+
+			if(isset($post_meta['tipo_contrato']) && $post_meta['tipo_contrato'][0] != "") {
+				echo " | Contrato: " . $post_meta['tipo_contrato'][0];
+			}
+		?>
+	</div>
+
+	<div class="col-xs-5 formacion">
+		<?php
+		if(isset($post_meta['formacion_minima']) && $post_meta['formacion_minima'][0] != "") {
+			echo "Carrera(s): <i>" . $post_meta['formacion_minima'][0] . "</i>";
+		}
+		?>
+	</div>
+
+</div>
+
+<?php							
+								$idx++;	
+
+							}
+
+		 				break;
+
+		 				} 
+	 				}
+	 			
+		  		?>
+				<?php get_contenido('nav'); ?>
+			</div>
+		</section>
 
 <?php get_footer(); ?>
